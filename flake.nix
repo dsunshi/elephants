6d1cce76b2198c5de8b2177aca310965705a85dc
D. Sunshine <david@sunshines.org>
2024-10-04 23:19:00 +0000

    initial cabal project

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [ inputs.haskell-flake.flakeModule ];
+      perSystem = { self', system, lib, config, pkgs, ... }: {
+        haskellProjects.default = {
+          devShell = { hlsCheck.enable = true; };
+
+          # What should haskell-flake add to flake outputs?
+          autoWire = [ "packages" "apps" "checks" ]; # Wire all but the devShell
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "my-project custom development shell";
+          inputsFrom = [ config.haskellProjects.default.outputs.devShell ];
+          nativeBuildInputs = with pkgs; [
+            haskell-language-server
+            haskellPackages.hoogle
+            haskellPackages.fast-tags
+            haskellPackages.haskell-debug-adapter
+            haskellPackages.haskell-dap
+            haskellPackages.ghci-dap
+            haskellPackages.threadscope
+            haskellPackages.markdown-unlit # Literate Haskell support for Markdown
+          ];
+        };
+      };
+    };
+}
